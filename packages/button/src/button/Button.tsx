a0fe70c4817285f3749a26c163089dff5b9939d1
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-07-17 17:07:14 +0300

    fix: add necessary dependencies

--- packages/button/src/button/Button.tsx
+++ packages/button/src/button/Button.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./button.module.scss";
 
 export type ButtonProps = {
   color?: string;
 };
 
-export const Button: React.FC<ButtonProps> = ({ color = "aqua" }) => {
-  return <button style={{ background: color }}>Rollup Button</button>;
+export const Button: React.FC<ButtonProps> = () => {
+  return <button className={styles.container}>Rollup Button</button>;
 };
